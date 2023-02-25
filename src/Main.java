6bca1e7102950c1c0bb0837484ed6c296b2bbf88
Danya <daniildocenko218@gmail.com>
2023-02-25 18:28:48 +0200

    second task

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,18 @@ public class Main {
     public static void main(String[] args) {
         String firstTask = "Bob";
         System.out.println(helloName(firstTask));
+        String secondTask1 = "Hello";
+        String secondTask2 = "Java";
+        System.out.println(makeAbba(secondTask1,secondTask2));
 
     }
     public static String helloName(String name) {
         return "Hello " + name + "!";
     }
+    public static String makeAbba (String a, String b) {
+        return a + b + b + a;
+    }
+
 
 
 }
