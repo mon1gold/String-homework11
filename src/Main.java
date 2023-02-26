a8361292e83cccdad0ec4ac4750b075157e07634
Danya <daniildocenko218@gmail.com>
2023-02-26 12:59:25 +0200

    firth task

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,43 @@
 public class Main {
 
     public static void main(String[] args) {
+        // 1 task
         String firstTask = "Bob";
         System.out.println(helloName(firstTask));
+        // 2 task
         String secondTask1 = "Hello";
         String secondTask2 = "Java";
         System.out.println(makeAbba(secondTask1,secondTask2));
+        // 3 task
+        String thirdA = "Java";
+        String thirdB = "Hello";
+        System.out.println(nonStart(thirdB, thirdA));
+        // 4 task
+        String firthTask = "badly";
+        System.out.println(endsLy(firthTask));
 
     }
     public static String helloName(String name) {
         return "Hello " + name + "!";
+
     }
     public static String makeAbba (String a, String b) {
         return a + b + b + a;
     }
+
+    public static String nonStart(String a, String b) {
+        String first = a.substring(1);
+        String second = b.substring(1);
+        String third  = first.concat(second);
+        return third;
+
+    }
+
+    public static boolean endsLy(String str) {
+        return str.endsWith("ly");
+    }
+
+
 
 
 
